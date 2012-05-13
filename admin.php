0febbedf09199890c8ad8b5dc6ca8f10e7eff079
unknown <Olivier@Wall-E.(none)>
2012-05-13 15:06:07 +0200

    Utilisation du nouveau Pager

--- admin.php
+++ admin.php
@@ -3,47 +3,56 @@
 include_once "environment.php";
 
 $page = new Pager('Admin');
+ob_start();
 $page->title = 'Administration';
 $page->pageTitle = 'Page d\'administration du site';
 
 // Traitement des soumissions des formulaires
-if ($page->visible() && $VARS->hasVars('post')) {
-	$DB->update('users', array(
-			'allowed_pages' => $VARS->post('userAccess'),
-			'admin' => ($VARS->post('admin', 'int'))
-		),
-		"id={$VARS->post('userId')}");
+if ($page->visible()) {
+	switch ($VARS->post('action')) {
+		case 'update':
+			$DB->update('users', array(
+					'allowed_pages' => $VARS->post('userAccess'),
+					'admin' => ($VARS->post('admin', 'int'))
+				),
+				"id={$VARS->post('userId')}");
+			break;
+	}
 }
 
 $page->addJs('javascript/admin.js');
 
-// Liste des pages et composants
-$listPages = '';
+?>
+<h2>Pages du site</h2>
+<ul id='pageList'>
+<?php 
 $DB->select('pages', 'id, title', '', array('orderBy'=>'id'));
 while ($pageItem = $DB->fetch()) {
-	$listPages.= "<li>[{$pageItem['id']}] - $pageItem[title]";
+	echo "<li>[{$pageItem['id']}] - $pageItem[title]";
 }
 $DB->endQuery();
+?>
+</ul>
 
+<?php 
 $usersForms = '';
 $DB->select('users', '*', '', array('orderBy'=>'login'));
 while ($user = $DB->fetch()) {
 	$form = new Form('userAccess');
 	$htmlForm = $form->create('', '', 'accessForm');
+	$htmlForm.= $form->hidden('action', 'update');
 	$htmlForm.= $form->hidden('userId', $user['id']);
 	$htmlForm.= $form->input('userAccess', $user['login'], $user['allowed_pages']).'</br>';
 	$htmlForm.= $form->check('admin', 'admin', 
 			array('1'=> '1'==$user['admin']));
-	$htmlForm.= $form->submit('update', 'Mettre à jour');
+	$htmlForm.= $form->submit('', 'Mettre à jour');
 	$htmlForm.= $form->end();
 	
 	$usersForms.= $htmlForm;
 }
 $DB->endQuery();
 
-$page->content = "<h2>Pages du site</h2>
-<ul id='pageList'>$listPages</ul>
-$usersForms";
+echo $usersForms;
 
 $page->render();
 
