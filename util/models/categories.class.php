44fc5db43aba563eebf81781ab34c6f61cc9fb46
unknown <Olivier@Wall-E.(none)>
2012-08-12 19:02:13 +0200

    Création d'un helper pour les catégories.

---
+++ util/models/categories.class.php
@@ -0,0 +1,33 @@
+<?php
+
+class Categories {
+	public static $categories = array();
+
+	public static function init($db) {
+		if (0 < $db->select('categories', 'id, categorie', '')) {
+			while ($data = $db->fetch()) {
+				Categories::$categories[$data['id']] = $data['categorie'];
+			}
+		}
+		$db->endQuery();
+	}
+
+	public static function id($categorie) {
+		foreach (Categories::$categories as $key => $value) {
+			if ($value == $categorie) {
+				return $key;
+			}
+		}
+		return -1;
+	}
+
+	public static function categorie($id) {
+		if (array_key_exists($id, Categories::$categories)) {
+			return Categories::$categories[$id];
+		}
+
+		return '';
+	}
+
+}
+
