56cc5e760cba8fb7406be33904eeb46781ac148f
unknown <Olivier@Wall-E.(none)>
2012-12-12 22:56:35 +0100

    Nourrit un peu le texte de la page d'accueil

--- accueil.php
+++ accueil.php
@@ -4,10 +4,11 @@ include_once "environment.php";
 
 $page = new Pager('Index');
 $page->headerTitle('Accueil');
-$page->pageTitle('Bienvenue sur la page d\'accueil'.($VISITOR->isLogged()? ', Olivier': ''));
+$page->pageTitle('Bonjour et bienvenue sur le site du mariage de Colombe & Olivier');
 ?>
 
-<p>Bonjour tout le monde</p>
+<p>Sur ce petit site, vous trouverez surtout des informations sur la salle, comment y aller, les bonnes adresses dans les alentours 
+	où vous pourrez dormir, ...</p>
 
 <?php
 $page->render();
