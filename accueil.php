1283866f9325b0bfec8dee3919ca62ff7dd8e1c1
unknown <Olivier@Wall-E.(none)>
2012-12-25 22:45:19 +0100

    Remplissage de la page d'accueil.

--- accueil.php
+++ accueil.php
@@ -4,11 +4,28 @@ include_once "environment.php";
 
 $page = new Pager('Accueil');
 $page->headerTitle('Accueil');
-$page->pageTitle('Bonjour et bienvenue sur le site du mariage de Colombe & Olivier');
+$page->pageTitle('Bienvenue sur le site du mariage de Colombe & Olivier');
+$page->addCss('css/accueil.css');
+
 ?>
 
-<p>Sur ce petit site, vous trouverez surtout des informations sur la salle, comment y aller, les bonnes adresses dans les alentours 
-	où vous pourrez dormir, ...</p>
+<p>
+	Sur ce petit site, vous trouverez surtout des informations sur la salle, comment y aller, les bonnes adresses dans les alentours 
+	où vous pourrez dormir, ...
+</p>
+
+<div>
+	<?php echo HtmlHelper::img("data/couple.png", array('title' => "Photo des futurs mariés à St-K", 'id'=> "nous")); ?>
+</div>
+
+<p>
+	En détail :
+	<ul>
+		<li>nos conseils sur la manière de venir, avec ou sans GPS;</li>
+		<li>nous vous conseillons une liste d'hôtels à proximité de la salle de réception;</li>
+		<li>des informations pratiques sur l'organisation de la soirée</li>
+	</ul>
+</p>
 
 <?php
 $page->render();
