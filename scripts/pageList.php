54034aa144061fba4f04ed5393dd44ea055cc626
Olivier <kineolyan+github@gmail.com>
2012-12-25 12:41:55 +0100

    Update scripts/pageList.php
    Ajoute le nom du fichier comme attribut.

--- scripts/pageList.php
+++ scripts/pageList.php
@@ -1,28 +1,37 @@
 <?php
 
 class Page {
+	private $_file;
 	private $_name;
 	private $_title;
 	
-	public function __construct($name, $title) {
+	public function __construct($file, $name, $title) {
+		$this->_file = $file;
 		$this->_name = $name;
 		$this->_title = $title;
 	}
 	
+	public function file() { return $this->_file; }
 	public function title() { return $this->_title; }
 	public function name() { return $this->_name; }
+	
+	public function fileName() {
+		$fileName = $this->_file;
+		$fileName[0] = strtolower($fileName[0]);
+		
+		return $fileName . '.php';
+	}
 }
 
-// $pagesList[<fichier>] = <titre>
 $pagesList = array(
-		'Index' => new Page('Entrée', "Lien vers l'image d'accueil"),
-		'Accueil' => new Page('Accueil', "Page d'accueil du site"),
-		'Infos' => new Page('Informations', "Informations diverses sur la réception"),
-		'Contact' => new Page('Contact', "Comment nous contacter"),
-		'Listing' => new Page('Liste des invités', "Gestion de la liste des invités"),
-		'Stats' => new Page('Statistiques', "Statistiques sur plusieurs points du mariage"),
-		'Facture' => new Page('Facture', "Facture et autres informations"),
-		'Admin' => new Page('Administration', "Zone d'administration")
+		new Page('Index', 'Entrée', "Lien vers l'image d'accueil"),
+		new Page('Accueil', 'Accueil', "Page d'accueil du site"),
+		new Page('Infos', 'Informations', "Informations diverses sur la réception"),
+		new Page('Contact', 'Contact', "Comment nous contacter"),
+		new Page('Listing', 'Liste des invités', "Gestion de la liste des invités"),
+		new Page('Stats', 'Statistiques', "Statistiques sur plusieurs points du mariage"),
+		new Page('Facture', 'Facture', "Facture et autres informations"),
+		new Page('Admin', 'Administration', "Zone d'administration")
 );
 
 $componentsList = array(
