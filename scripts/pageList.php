bb3f9bb389467ab15bc31f4254cd99b4f0b73e1e
Olivier <kineolyan+github@gmail.com>
2012-12-25 12:34:36 +0100

    Update scripts/pageList.php
    Ajout d'une structure de page pour mieux décrire chaque page

--- scripts/pageList.php
+++ scripts/pageList.php
@@ -1,16 +1,28 @@
 <?php
 
+class Page {
+	private $_name;
+	private $_title;
+	
+	public function __construct($name, $title) {
+		$this->_name = $name;
+		$this->_title = $title;
+	}
+	
+	public function title() { return $this->_title; }
+	public function name() { return $this->_name; }
+}
+
 // $pagesList[<fichier>] = <titre>
 $pagesList = array(
-		'Index' => 'Entrée',
-		'Accueil' => 'Accueil',
-		'Infos' => 'Informations',
-		'Contact' => 'Contact',
-		'Listing' => 'Liste des invités',
-		'Stats' => 'Statistiques',
-		'Facture' => 'Facture',
-		'Admin' => 'Administration',
-		'Contact' => 'Contact'
+		'Index' => new Page('Entrée', "Lien vers l'image d'accueil"),
+		'Accueil' => new Page('Accueil', "Page d'accueil du site"),
+		'Infos' => new Page('Informations', "Informations diverses sur la réception"),
+		'Contact' => new Page('Contact', "Comment nous contacter"),
+		'Listing' => new Page('Liste des invités', "Gestion de la liste des invités"),
+		'Stats' => new Page('Statistiques', "Statistiques sur plusieurs points du mariage"),
+		'Facture' => new Page('Facture', "Facture et autres informations"),
+		'Admin' => new Page('Administration', "Zone d'administration")
 );
 
 $componentsList = array(
