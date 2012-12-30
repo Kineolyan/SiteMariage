ff395133d4f9d5348417b4f8b0c615ec44292f98
unknown <Olivier@Wall-E.(none)>
2012-12-30 16:36:45 +0100

    Remplit l'onglet des logements.

--- vues/include/loger.php
+++ vues/include/loger.php
@@ -1,3 +1,126 @@
 <p>
-	Des logements sont disponibles à proximité du lieu de réception. La liste sera prochainement disponible sur le site.
+	Des logements sont disponibles à proximité du lieu de réception. Certes, ce n'est pas notre liste d'hôtels, mais ceux conseillés par
+	l'agence nous louant la salle de la réception.<br/>
+	Nous vous proposons donc une sélection pour tous les goûts : des hôtels, des chambres d'hôtes, et pour les courageux aventuriers, la pelouse autour de la salle pour 
+	<?php Html::tip('planter la tente', 'Nous vous rapellons que le mariage a lieu en avril, un mois où le temps est incertain. Les proverbes signalent que en avril, il ne faut pas se découvrir d\' un fil.'
+		.' Pour votre santé, nous vous conseillons donc de consulter des sites spécialisés à même de vous donner de bonnes prévisions météorologiques. Vous pouvez égalemement faire ces calculs par vous-même mais'
+		.' nous vous invitons alors à le vérifier plusieurs fois, notamment l\'homogénéité.'); ?>.
 </p>
+
+<h2>Hôtels</h2>
+<div class='hotel'>
+	<h3>Les Bords de Seine</h3>
+	<p>
+		<span class="distance">A 8 km</span>
+		<span class="adresse">Rue du Docteur Duval, 957800 La Roche Guyon</span>
+		<span class="contact">Téléphone : <?php echo Utils::telephone('0130983552'); ?></span>
+		<span class="prix">De 47 à 66€, petit déj à 7€</span>
+	</p>
+</div>
+
+<div class='hotel'>
+	<h3>Val de Seine</h3>
+	<p>
+		<span class="distance">A 14 km</span>
+		<span class="adresse">Rue Marcel Tabu, 78200 Mantes la Jolie</span>
+		<span class="contact">Téléphone : <?php echo Utils::telephone('0130330370'); ?></span>
+		<span class="prix">de 53 à 69€, petit déj à 7€</span>
+	</p>
+</div>
+
+<div class='hotel'>
+	<h3>Hotel Eclipse</h3>
+	<p>
+		<span class="distance">A 17 km</span>
+		<span class="adresse">Rue Pierettes,78200 Magnanville</span>
+		<span class="contact">Téléphone : <?php echo Utils::telephone('0130984141'); ?></span>
+		<span class="prix">65€ la nuit et petit déj à 8€</span>
+	</p>
+</div>
+
+<div class='hotel'>
+	<h3>Formule 1</h3>
+	<p>
+		<span class="distance">A 17 km</span>
+		<span class="adresse">Rue pincevins, 78200 Magnanville</span>
+		<span class="contact">Téléphone : <?php echo Utils::telephone('0891705293'); ?></span>
+		<span class="prix">à partir de 28€</span>
+	</p>
+</div>
+
+<div class='hotel'>
+	<h3>Hôtel Mercure</h3>
+	<p>
+		<span class="distance">A 33 km</span>
+		<span class="adresse">3 rue des Chênes Emeraudes, 95000 Cergy</span>
+		<span class="contact">Téléphone : <?php echo Utils::telephone('0134249494'); ?></span>
+		<span class="prix">130 à 150€, petit déj à 12,5€</span>
+	</p>
+</div>
+
+<div class='hotel'>
+	<h3>Château de l'Hermitage</h3>
+	<p>
+		<span class="distance">A 42 km</span>
+		<span class="adresse">93500 Ennery</span>
+		<span class="contact">
+			Téléphone : <?php echo Utils::telephone('0134439061');?>, et par Internet sur 
+			<? echo Html::link('www.chateaude lhermitage.fr', 'http://www.chateaude lhermitage.fr');?></span>
+		<span class="prix">140 à 250€ et un petit déj à 12€</span>
+	</p>
+</div>
+
+<h2 class="cleaner">Chambres d'hôtes</h2>
+
+<div class='hotel'>
+	<h3>M et Mme TESSIER</h3>
+	<p>
+		<span class="adresse">95510 Villers en Arthies</span>
+		<span class="contact">Téléphone : <?php echo Utils::telephone('0134782058');?></span>
+	</p>
+</div>
+
+<div class='hotel'>
+	<h3>La Maison des Cool Heures<br/>Patricia et Yvon LEROUX</h3>
+	<p>
+		<span class="distance">A 1,5 km</span>
+		<span class="adresse">23, Chemin de la vallée, 95510 Vienne en Arthies</span>
+		<span class="contact">Téléphone : <?php echo Utils::telephone('0611766745');?> et <?php echo Utils::telephone('0608270421');?></span>
+	</p>
+</div>
+
+<div class='hotel'>
+	<h3>Mme HAZARD</h3>
+	<p>
+		<span class="distance">A 4 km</span>
+		<span class="adresse">95510 Chérence</span>
+		<span class="contact">Téléphone : <?php echo Utils::telephone('0134782318');?></span>
+	</p>
+</div>
+
+<div class='hotel'>
+	<h3>M HUBER</h3>
+	<p>
+		<span class="distance">A 4 km</span>
+		<span class="adresse">95510 Chérence</span>
+		<span class="contact">Téléphone : <?php echo Utils::telephone('0134782637');?></span>
+	</p>
+</div>
+
+<div class='hotel'>
+	<h3>Chambre d'hôtes Saint Denis</h3>
+	<p>
+		<span class="distance">A 4 km</span>
+		<span class="adresse">95510 Chérence</span>
+		<span class="contact">Téléphone : <?php echo Utils::telephone('0134781502');?> et <?php echo Utils::telephone('0648261600');?></span>
+	</p>
+</div>
+
+<div class='hotel'>
+	<h3>M et Mme PETIT HALGATE</h3>
+	<p>
+		<span class="distance">A 5 km</span>
+		<span class="adresse">95510 Vétheuil</span>
+		<span class="contact">Téléphone : <?php echo Utils::telephone('0624685625');?></span>
+	</p>
+</div>
