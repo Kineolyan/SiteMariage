7a688f6e2e110bd9aa82d0a2219770b4a7c826af
unknown <Olivier@Wall-E.(none)>
2012-12-30 19:22:37 +0100

    Ajout d'un tableau de stats de presence par categorie

--- stats.php
+++ stats.php
@@ -5,6 +5,45 @@ include_once "environment.php";
 $page = new Pager('Stats');
 $page->headerTitle('Statistiques');
 $page->pageTitle('Statistiques sur les invités');
+$page->addCss('css/stats.css');
+
+function createStatsArray() {
+	return  array(
+		'total' => 0,
+		Invite::getStatus(1) => 0,
+		Invite::getStatus(-1) => 0,
+		Invite::getStatus(0) => 0
+	);
+}
+
+function sumStats(&$total, $addition) {
+	foreach ($addition as $key => $value) {
+		$total[$key] += $value;
+	}
+}
+
+function getStatsForCategorie($categorieId) {
+	global $DB;
+
+	$DB->select('categories JOIN mm_user_categorie ON mm_user_categorie.categorie_id=categories.id'
+			.	' JOIN invites ON mm_user_categorie.user_id=invites.id',
+		'COUNT(mm_user_categorie.id) AS nb, invites.statut AS statut', 
+		'mm_user_categorie.categorie_id='.$categorieId,
+		array('groupBy' => 'invites.statut'));
+
+	$stats = createStatsArray();
+
+	while ($stat = $DB->fetch()) {
+		$count = intval($stat['nb']);
+
+		$stats[Invite::getStatus($stat['statut'])] = $count;
+		$stats['total'] += $count;
+	}
+
+	$DB->endQuery();
+
+	return $stats;
+}
 
 // Statistiques sur les réponses
 $DB->select('invites', 'COUNT(*) as total, statut', '', array('groupBy' => 'statut'));
@@ -29,19 +68,40 @@ Réponses attendues: <?php echo $statutStats['0']; ?>
 </p>
 
 <h2>Catégories</h2>
+
+<table class="table table-hover">
+	<thead>
+		<tr>
+			<th>Catégorie</th>
+			<th><?php echo Invite::getStatus(1);?></th>
+			<th><?php echo Invite::getStatus(-1);?></th>
+			<th><?php echo Invite::getStatus(0);?></th>
+			<th>Total</th>
+		</tr>
+	</thead>
+	<tbody>
 <?php
-$DB->select('categories JOIN mm_user_categorie ON mm_user_categorie.categorie_id=categories.id',
-		'categorie, COUNT(mm_user_categorie.id) as nb', '',
-		array('groupBy' => 'categories.id'));
-$listeCategories = '';
-while ($categorieData = $DB->fetch()) {
-	$listeCategories .= "<li class=\"stats\">$categorieData[categorie] : <span class=\"stats\">$categorieData[nb]</span></li>";
+$globalStats = createStatsArray();
+
+foreach (Categories::getCategories() as $categorieId => $categorie) {
+	$stats = getStatsForCategorie($categorieId);
+
+	echo sprintf('<tr> <td>%s</td> <td>%d</td> <td>%d</td> <td>%d</td> <td class="totalCategorie">%d</td> </tr>',
+		$categorie, 
+		$stats[Invite::getStatus(1)], $stats[Invite::getStatus(-1)], $stats[Invite::getStatus(0)], 
+		$stats["total"]);
+
+	sumStats($globalStats, $stats);
 }
-$DB->endQuery();
+
+echo sprintf("<tr class='totalPresence'> <td>Total par présence</td> <td>%d</td> <td>%d</td> <td>%d</td> <td>%d</td> </tr>",
+		$globalStats[Invite::getStatus(1)],
+		$globalStats[Invite::getStatus(-1)], 
+		$globalStats[Invite::getStatus(0)], 
+		$globalStats["total"]);
 ?>
-<p>Groupements par catégories :
-	<ul><?php echo $listeCategories; ?></ul>
-</p>
+	</tbody>
+</table>
 
 <?php
 $page->render();
