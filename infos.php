64b4e794ac4c1fcef946b99fa9ef48b480d4afc6
Olivier <kineolyan@hotmail.fr>
2013-02-16 00:17:16 +0100

    Rend les onglets accessibles via des liens url.

--- infos.php
+++ infos.php
@@ -8,6 +8,8 @@ $page->pageTitle('Informations pratiques');
 $page->addJs(array('javascript/maps.js', 'javascript/infos.js'));
 $page->addCss("css/infos.css");
 
+$VARS->setDefaultGet('tab', 'venir');
+
 ?>
 
 <div id="pageInfos">
@@ -18,29 +20,29 @@ $page->addCss("css/infos.css");
   </p> -->
 
   <div class="tabbable">
-    <ul class="nav nav-tabs">
-      <li class="active"><a href="#tab1" data-toggle="tab">Comment venir ?</a></li>
-      <li><a href="#tab2" data-toggle="tab">Où dormir</a></li>
-      <li><a href="#tab3" data-toggle="tab">Sur place</a></li>
-      <li><a href="#tab4" data-toggle="tab">Liste de mariage</a></li>
-    </ul>
+    <?php Html::tabNavigation(array(
+      'venir' => "Comment venir ?",
+      'logement' => "Où dormir ?",
+      'reception' => "Sur place",
+      'liste' => "Liste de mariage"
+    )); ?>
     <div class="tab-content">
-      <div class="tab-pane active" id="tab1">
+      <div class="tab-pane <?php Html::activateTab('venir'); ?>" id="venir">
         <!-- Comment venir -->
         <?php include_once "vues/include/venir.php"; ?>
       </div>
 
-      <div class="tab-pane" id="tab2">
+      <div class="tab-pane <?php Html::activateTab('logement'); ?>" id="logement">
       	<!-- Où se loger -->
         <?php include_once "vues/include/loger.php"; ?>
       </div>
 
-      <div class="tab-pane" id="tab3">
+      <div class="tab-pane <?php Html::activateTab('reception'); ?>" id="reception">
         <!-- Sur place -->
         <?php include_once "vues/include/sur_place.php"; ?>
       </div>
 
-      <div class="tab-pane" id="tab4">
+      <div class="tab-pane <?php Html::activateTab('liste'); ?>" id="liste">
         <!-- Liste de mariage -->
         <?php include_once "vues/include/liste_mariage.php"; ?>
       </div>
